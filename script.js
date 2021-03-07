69b6a5914c0c2dcadf0ed4e3033dcc10d7c7fb20
Bipin <bipp2009@gmail.com>
2021-03-07 11:31:08 +0000

    Added character validation and getting the input from user for these

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "1234567890";
+var special = "<>?/)(*&^%$£@";
+
+var userWantsUpper = confirm("Would you want Upper Case Characters in your password?")
+var userWantsLower = confirm("Would you want Lower Case Characters in your password?")
+var userWantsNumbers = confirm("Would you want Numbers in your password?")
+var userWantsSpecials = confirm("Would you want Special Characters in your password?")
+
+if (userWantsUpper === false && userWantsLower === false && userWantsSpecials === false && userWantsNumbers === false){
+  alert("Password not suitable: Please select at least one option for character types");
+  return ""; 
+}
+
+  return "password"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

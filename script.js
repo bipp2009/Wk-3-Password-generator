9b3a6720e74bb14c81217b7350df25a804c589a3
Bipin <bipp2009@gmail.com>
2021-03-07 11:37:22 +0000

    Added validation for length of password etc.

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
+  //Declare variables needed to create the final password
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "1234567890";
 var special = "<>?/)(*&^%$£@";
 
+//Get the characters that a user wants and validate
 var userWantsUpper = confirm("Would you want Upper Case Characters in your password?")
 var userWantsLower = confirm("Would you want Lower Case Characters in your password?")
 var userWantsNumbers = confirm("Would you want Numbers in your password?")
@@ -15,6 +17,14 @@ var userWantsSpecials = confirm("Would you want Special Characters in your passw
 if (userWantsUpper === false && userWantsLower === false && userWantsSpecials === false && userWantsNumbers === false){
   alert("Password not suitable: Please select at least one option for character types");
   return ""; 
+}
+// Get the length of password that the user wants and validate
+var lengthOfPasswordString = prompt("What length of password would you like?");
+var lengthOfPasswordNumber = parseInt(lengthOfPasswordString);
+
+if (lengthOfPasswordNumber < 8 || lengthOfPasswordNumber > 128 ){
+  alert("Password not suitable: Please enter a number greater than 8 and less than 128");
+  return "";
 }
 
   return "password"
